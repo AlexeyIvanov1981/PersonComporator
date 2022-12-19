d9bac4bdf512dd653bc3173f00b8ee00d6cd1387
AlexeyIvanov1981 <118156280+AlexeyIvanov1981@users.noreply.github.com>
2022-12-19 13:06:32 +0300

    Update PersonComporator.java

--- src/PersonComporator.java
+++ src/PersonComporator.java
@@ -1,2 +1,21 @@
-public class PersonComporator {
+import java.util.Comparator;
+
+public class PersonComporator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
+        int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
+
+        if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2){
+            return -1;
+        } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2){
+            return +1;
+        } else if (o1.getAge() > o2.getAge()){
+            return -1;
+        } else if (o1.getAge() < o2.getAge()) {
+            return +1;
+        }
+        return 0;
+    }
 }
