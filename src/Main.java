3d499f4b5668bf6d13d7fe01a397bdf5535f9c75
AlexeyIvanov1981 <118156280+AlexeyIvanov1981@users.noreply.github.com>
2022-12-19 13:07:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> person = new ArrayList<>();
-        person.add(new Person("Alex", "Iv", 42));
-        person.add(new Person("Vova", "Pit", 23));
+        person.add(new Person("Alex", "Iv Iv iv", 42));
         person.add(new Person("Senya", "Lom", 50));
-        person.add(new Person("Grisha", "Git", 15));
+        person.add(new Person("Grisha", "Git Kit Mit", 15));
         person.add(new Person("Sasha", "Luk", 17));
-        person.add(new Person("Tolya", "Rub", 42));
-        person.add(new Person("Bolik", "Bol", 35));
+
+        Collections.sort(person, new PersonComporator());
 
         System.out.println(person);
     }
