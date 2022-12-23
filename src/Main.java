701e61c2580f3fc2923f0bf59cedca2531c87ecf
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-12-23 15:53:30 +0300

    without PersonComparator class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -11,7 +12,24 @@ public class Main {
         person.add(new Person("Sasha", "Luk", 17));
         person.add(new Person("Pasha", "Git Kit Mit Fit", 99));
 
-        Collections.sort(person, new PersonComparator());
+        Collections.sort(person, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
+                int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
+
+                if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2) {
+                    return -1;
+                } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2) {
+                    return +1;
+                } else if (o1.getAge() > o2.getAge()) {
+                    return -1;
+                } else if (o1.getAge() < o2.getAge()) {
+                    return +1;
+                }
+                return 0;
+            }
+        });
 
         System.out.println(person);
     }
