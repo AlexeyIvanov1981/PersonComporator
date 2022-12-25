82d32b22f8baf8182ef670cefbe893bdda62f62f
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-12-25 15:20:54 +0300

    with PersonComparator class

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,7 @@ public class Main {
         person.add(new Person("Sasha", "Luk", 17));
         person.add(new Person("Pasha", "Git Kit Mit Fit", 99));
 
-        Collections.sort(person, new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
-                int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
-
-                if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2) {
-                    return -1;
-                } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2) {
-                    return +1;
-                } else if (o1.getAge() > o2.getAge()) {
-                    return -1;
-                } else if (o1.getAge() < o2.getAge()) {
-                    return +1;
-                }
-                return 0;
-            }
-        });
+        Collections.sort(person, new PersonComparator());
 
         System.out.println(person);
     }
