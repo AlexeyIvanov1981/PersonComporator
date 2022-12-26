41b0ff1fa4a09066fe09ad40cee0d906b6f3ef8f
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2022-12-26 14:31:50 +0300

    fix in main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +13,22 @@ public class Main {
         person.add(new Person("Sasha", "Luk", 17));
         person.add(new Person("Pasha", "Git Kit Mit Fit", 99));
 
-        Collections.sort(person, new PersonComparator());
+        Collections.sort(person, (o1, o2) -> {
+            int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
+            int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
+
+            if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2) {
+                return -1;
+            } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2) {
+                return +1;
+            } else if (o1.getAge() > o2.getAge()) {
+                return -1;
+            } else if (o1.getAge() < o2.getAge()) {
+                return +1;
+            }
+            return 0;
+        });
+
 
         System.out.println(person);
     }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,21 +1,21 @@
-import java.util.Comparator;
-
-public class PersonComparator implements Comparator<Person> {
-
-    @Override
-    public int compare(Person o1, Person o2) {
-        int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
-        int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
-
-        if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2){
-            return -1;
-        } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2){
-            return +1;
-        } else if (o1.getAge() > o2.getAge()){
-            return -1;
-        } else if (o1.getAge() < o2.getAge()) {
-            return +1;
-        }
-        return 0;
-    }
-}
+//import java.util.Comparator;
+//
+//public class PersonComparator implements Comparator<Person> {
+//
+//    @Override
+//    public int compare(Person o1, Person o2) {
+//        int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
+//        int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
+//
+//        if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2){
+//            return -1;
+//        } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2){
+//            return +1;
+//        } else if (o1.getAge() > o2.getAge()){
+//            return -1;
+//        } else if (o1.getAge() < o2.getAge()) {
+//            return +1;
+//        }
+//        return 0;
+//    }
+//}
