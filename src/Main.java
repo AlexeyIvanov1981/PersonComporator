7c854b4d451ac7edbe6e893f642de308b575098c
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2022-12-26 14:15:54 +0300

    fix in lambda

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,21 +13,8 @@ public class Main {
         person.add(new Person("Sasha", "Luk", 17));
         person.add(new Person("Pasha", "Git Kit Mit Fit", 99));
 
-        Collections.sort(person, (o1, o2) -> {
-            int numberOfWordsInSurnameO1 = o1.getSurname().split(" ").length;
-            int numberOfWordsInSurnameO2 = o2.getSurname().split(" ").length;
 
-            if (numberOfWordsInSurnameO1 > numberOfWordsInSurnameO2) {
-                return -1;
-            } else if (numberOfWordsInSurnameO1 < numberOfWordsInSurnameO2) {
-                return 1;
-            } else if (o1.getAge() > o2.getAge()) {
-                return -1;
-            } else if (o1.getAge() < o2.getAge()) {
-                return 1;
-            }
-            return 0;
-        });
+        person.removeIf(person1 -> person1.getAge() < 18);
 
         System.out.println(person);
     }
